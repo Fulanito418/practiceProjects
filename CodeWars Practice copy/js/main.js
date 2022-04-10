b42e17927c157216c77dd48ab3ebf09a80872c66
Alex Perez <perez418@gmail.com>
2022-04-09 22:25:14 -0400

    name shuffler lastFirst

--- CodeWars Practice copy/js/main.js
+++ CodeWars Practice copy/js/main.js
@@ -1,3 +1,23 @@
+//nameShuffler('john McClane'); => "McClane john"
+
+//let str = 'john McClane'
+
+// function nameShuffler(str){
+//   let strArr = str.split(' ')
+//   let lastFirst = [strArr[1], strArr[0]]
+//   return lastFirst.join(' ')
+// }
+
+
+//reverse the elements using the following code
+// function nameShuffle(str){
+//   return str.split(' ').reverse()
+// }
+
+// console.log(nameShuffle(str))
+
+
+
 /*
 //Coctail API code
 
@@ -29,6 +49,8 @@ function getDrink() {
 //Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text.
 //Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
 
+
+/*
 let numbersList = [0,1,2,3,4,5,6,7,8,9];
 // function isNotNumber(value) {
 //   return value = numbersList
