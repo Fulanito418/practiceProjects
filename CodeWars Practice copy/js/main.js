fdec67ec7e93ae703b5824e7bedd3d7b2bc20685
Alex Perez <perez418@gmail.com>
2022-04-13 22:35:49 -0400

    letter, number or character

--- CodeWars Practice copy/js/main.js
+++ CodeWars Practice copy/js/main.js
@@ -1,4 +1,37 @@
 
+
+// Write a function that will check if two given characters are the same case.
+
+// If either of the characters is not a letter, return -1
+// If both characters are the same case, return 1
+// If both characters are letters, but not the same case, return 0
+
+
+function sameCase(a, b){
+  aUpper = a.toUpperCase()
+  aLower = a.toLowerCase()
+  bUpper = b.toUpperCase()
+  bLower = b.toLowerCase()
+
+  if (a.toUpperCase() === a.toLowerCase() || b.toUpperCase() === b.toLowerCase()){
+    return -1
+  } else if ( (a === a.toUpperCase() && b === b.toUpperCase())
+  || (a === a.toLowerCase() && b === b.toLowerCase())) {
+  return 1;
+} else {
+  return 0
+}
+}
+
+console.log( sameCase('10','y') )
+
+
+
+
+
+
+
+/*
 let size = 3;
 
 function stringy(size) {
