8859051763af6ac417b3d79d1d1dd4ac57ba8f6e
Alex Perez <perez418@gmail.com>
2022-04-21 19:50:36 -0400

    Barista Solution

--- CodeWars Practice copy/js/main.js
+++ CodeWars Practice copy/js/main.js
@@ -2,12 +2,24 @@
 //If you have three customers with times [4,3,2], the first customer is going to wait 4 minutes for his coffee, the second customer is going to wait 4 minutes (the time needed for the first customer to get his coffee), another 2 minutes (the time needed to clean the machine) and 3 more minutes (the time you need to brew his coffee), so in total 9 minutes. The third customer, by the same logic, is about to wait 9 minutes (for the first two customers) + 2 more minutes(cleaning) + 2 minutes (his coffee brewing time). This order of brewing the coffee will end up in a total waiting time of 26 minutes, but note that this may not be the minimum time needed. This time depends on the order you choose to brew the cups of coffee.
 
 
-coffeesList = [3,2,5,10,9]
+let coffees = [2, 3, 5, 9, 10].sort((a, b) => a - b);
 
 
 
+let multiplier = coffees.length
+
+let sum = 0;
+
+for (i = 0; i < coffees.length; i++){
+  let coffeeTime = coffees[i] * multiplier
+  sum += coffeeTime + (2*i)
+  multiplier -= 1
+}
+
+console.log(sum)
 
 
+/*
 function barista(coffees){
 
   if (coffees.length<1){
