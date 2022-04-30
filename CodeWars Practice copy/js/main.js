d0d32235724e8aa1d0516bcef85742201c03e2f2
Alex Perez <perez418@gmail.com>
2022-04-29 23:43:49 -0400

    Grasshopper - Summation

--- CodeWars Practice copy/js/main.js
+++ CodeWars Practice copy/js/main.js
@@ -1,4 +1,14 @@
 
+//Grasshopper - Summation
+var summation = function (num) {
+  let sum = 0
+  for (i=1; i<=num; i++) {
+    sum += i
+  }
+  return sum
+}
+
+/*
 
 //Reversed Strings
 function solution(str){
