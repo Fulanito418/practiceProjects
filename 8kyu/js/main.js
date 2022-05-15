e893cf1c2bc34f4e78e46c89d614dc951d600226
Alex Perez <perez418@gmail.com>
2022-05-14 23:52:55 -0400

    added sub folders for kyus

---
+++ 8kyu/js/main.js
@@ -0,0 +1,14 @@
+//8Kyu
+
+
+
+//Convert a Boolean to a String
+//PREP
+//p: only valid inputs... covert a given boolean into its string representation
+//r: return the string equivalent
+//e: true => 'true'
+//p: ternary condition, if true return 'true' : 'false'
+
+function booleanToString(b){
+  return b ? 'true' : 'false'
+}
