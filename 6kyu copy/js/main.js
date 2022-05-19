b53e998904b35c6a39000e2d379f068f06027193
Alex Perez <perez418@gmail.com>
2022-05-18 23:21:04 -0400

    Shortest steps to a number progress

---
+++ 6kyu copy/js/main.js
@@ -0,0 +1,26 @@
+//6kyu
+
+//prep
+//p: a step can be defined as +1 or *2. Starts from 1 
+//r: return shortest number of steps to land on num
+//e: to reach 3 >>  1 + 1 + 1 takes two steps 
+// to reach 5 >> 1 *2 *2 + 1 takes three steps
+//p: create a counter and +1 each additional loop
+//loop *2 without going over
+
+
+function shortestStepsToNum(num) {
+  let counter = 0
+  let maxNum = num
+  let startNum = 1
+
+  for (let i = 1; startNum*2 < maxNum; i ++) {
+    console.log(`loop: ${i} counter: ${counter} startNum: ${startNum}`);
+    startNum *= 2
+    counter += 1
+  }
+  console.log(`ending counter: ${counter}`);
+  console.log(`ending startNum: ${startNum}`); 
+}
+
+shortestStepsToNum(10)
