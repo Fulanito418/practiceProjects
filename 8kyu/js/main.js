c3ea1832be8d39834c0317b3aa5a708e0af2d5f1
Alex Perez <perez418@gmail.com>
2022-05-18 23:27:54 -0400

    What is between?

--- 8kyu/js/main.js
+++ 8kyu/js/main.js
@@ -1,6 +1,23 @@
 //8Kyu
 
+//
+//PREP
+//P: integers from a to b where a<b
+//R: return an array with allintegers from a to b, inclusive
+//E: 5,10 return [5,6,7,8,9,10]
+//P: for loop to push a through b to an empty array
+// start at a and end when <= b
 
+//What is between?
+function between(a, b) {
+  let numArr =[];
+  for (let i = a; i <= b; i++){
+    numArr.push(i)
+  }
+  return numArr;
+}
+
+/*
 
 
 //Volume of a Cuboid
@@ -21,3 +38,4 @@ class Kata {
 function booleanToString(b){
   return b ? 'true' : 'false'
 }
+*/
