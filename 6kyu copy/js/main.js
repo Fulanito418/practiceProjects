ee469cd46586ff33d3e1cdcde4cb3ca55570ddc4
Alex Perez <perez418@gmail.com>
2022-06-26 00:12:50 -0400

    Multiples of 3 or 5 done

--- 6kyu copy/js/main.js
+++ 6kyu copy/js/main.js
@@ -1,12 +1,35 @@
 //6kyu
 
-
-
 //Multiples of 3 or 5
-function solution(number){
-  
-}
+//p given a natural number
+//r return a number representing the sum of all the numbers
+//below the given value that are multiples of 3 or 5
+//e  10 >> 3 + 5 + 6 + 9 = 23
+//p
 
+function solution(number){
+  //variable to keep track of added values
+  //empty array to push values
+  //let numArr = []
+  let counter = 0;
+  
+  for (let i = 1; i < number; i++ ) {    
+    //if number % 3 or 5 is zero, then add to counter
+    if(i % 3 == 0 || i % 5 == 0) {
+      //console.log(i)
+      //numArr.push(i)
+      counter+= i
+    }
+      
+    }
+    //console.log(numArr)
+    return counter
+  }
+
+console.log(solution(10))
+
+
+/*
 //Shortest steps to a number
 //prep
 //p: a step can be defined as +1 or *2. Starts from 1 
@@ -32,3 +55,4 @@ function shortestStepsToNum(num) {
 }
 
 shortestStepsToNum(10)
+*/
