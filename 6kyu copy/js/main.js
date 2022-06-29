96dc8f2b99aa125ceb903c3cead0d768b4dcc2d1
Alex Perez <perez418@gmail.com>
2022-06-29 00:12:57 -0400

    Who likes it? done

--- 6kyu copy/js/main.js
+++ 6kyu copy/js/main.js
@@ -1,11 +1,32 @@
 //6kyu
 
 
-//Who likes it?
-function likes(names) {
-  // TODO
-}
 
+
+//Who likes it?
+//p: given an array with names, can include 0 values
+//r: return string
+//e: ['Max', 'Phil'] >> 'Max and Phil like this'
+//p
+
+function likes(names) {
+  //store length of names array
+  let namesLength = names.length
+  //console.log(namesLength)
+  //if namesLength is 0 >> 'no one likes thsi'
+  if (namesLength == 0) {
+    return 'no one likes this'
+    console.log('no one likes this')
+  } else if (namesLength == 1) {
+    // return `${names[0]} likes this`
+  } else if (namesLength == 2) {
+    // return `${names[0]} and ${names[1]} like this`
+  } else if (namesLength == 3) {
+    //return `${names[0]}, ${names[1]} and ${names[2]} like this`
+  } else {
+    //return `${names[0]}, ${names[1]} and ${namesLength - 2} others like this`
+  }
+}
 
 
 /*
