475f72a76ede4eb162b3693fc802e5d649743916
Alex Perez <perez418@gmail.com>
2022-09-06 18:27:43 -0400

    Running Sum of 1d Array

--- leetcode/js/main.js
+++ leetcode/js/main.js
@@ -1,6 +1,56 @@
 //leetcode
 
 
+
+//Running Sum of 1d Array
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums. 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+// Example 2:
+
+// Input: nums = [1,1,1,1,1]
+// Output: [1,2,3,4,5]
+// Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+// Example 3:
+
+// Input: nums = [3,1,2,10,1]
+// Output: [3,4,6,16,17]
+
+// Constraints:
+
+// 1 <= nums.length <= 1000
+// -10^6 <= nums[i] <= 10^6
+
+// p given an array of numbers
+// r return an array
+// e [1,2,3,4,5] >> [1,3,6,10,15]
+// p
+
+var runningSum = function(nums) {
+    //variable to hold sum
+    let sum = 0;
+    //sumsArr
+    let sumArr = [];
+    //push running sum into sumsArr
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i]
+        sumArr.push(sum)
+    }
+    return sumArr
+
+};
+
+
+
+
+
+/*
 //20. Valid Parentheses
 // Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
