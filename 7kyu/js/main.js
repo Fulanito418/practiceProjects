7ba11ae41e80a55c5bf185a92542454d0443b8bc
Alex Perez <perez418@gmail.com>
2022-10-04 10:34:55 -0400

    Is this a triangle?

--- 7kyu/js/main.js
+++ 7kyu/js/main.js
@@ -1,6 +1,27 @@
 //7Kyu
 
+//Is this a triangle?
+function isTriangle(a,b,c)
+{
+   if (a*b*c == 0) {
+     return false
+   }
+  
+  if (
+    a + b > c &&
+    a + c > b &&
+    b + c > a) {
+    
+    return true 
+      
+    } else {
+      
+      return false
+    }
+    
+  }
 
+/*
 //reverse list
 function reverseList(arr) {
   return arr.reverse();
--- leetcode/js/main.js
+++ leetcode/js/main.js
@@ -1,11 +1,52 @@
 //leetcode
 
 
+
+const App = () => {
+    const options = [
+      { label: 'Fruit', value: 'fruit' },
+      { label: 'Vegetable', value: 'vegetable' },
+      { label: 'Meat', value: 'meat' },
+    ];
+  
+    const [value, setValue] = React.useState('fruit');
+  
+    const handleChange = (event) => {
+      setValue(event.target.value);
+    };
+  
+    return (
+      <div>
+        <Dropdown
+          label="What do we eat?"
+          options={options}
+          value={value}
+          onChange={handleChange}
+        />
+  
+        <p>We eat {value}!</p>
+      </div>
+    );
+  };
+
+  const Dropdown = ({ label, value, options, onChange }) => {
+    return (
+      <label>
+        {label}
+        <select value={value} onChange={onChange}>
+          {options.map((option) => (
+            <option value={option.value}>{option.label}</option>
+          ))}
+        </select>
+      </label>
+    );
+  };
+
+
+
+/*
 //Longest Substring Without Repeating Characters
-/**
- * @param {string} s
- * @return {number}
- */
+
  var lengthOfLongestSubstring = function(s) {
     
     //create array
