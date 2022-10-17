788c5f07cb8bbb574dd8ac81408e5c8d6ff4a290
Alex Perez <perez418@gmail.com>
2022-10-17 12:43:14 -0400

    First Bad Version

--- dsapractice/js/main.js
+++ dsapractice/js/main.js
@@ -1,6 +1,37 @@
 //DSA
 
+//First Bad Version
+var solution = function(isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function(n) {
+      let left = 1;
+      let right = n;
+      let mid;
+      
+      while (left <= right) {
+          
+          mid = left + Math.floor((right-left)/2)
+      
+          if (isBadVersion(mid)) {
+              
+              if (isBadVersion(mid-1)) {
+                  right = mid - 1   
+              } else {
+                  return mid    
+              }
+              
+          } else {
+              left = mid + 1
+          }
+  }
+}
+}
 
+
+/*
 //Binary Search
 let search = function (nums, target) {
 	
