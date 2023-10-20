2747ca2f3c20ec9a9586bd3b8f53eb0d871c6b72
Alex Perez <63934973+Fulanito418@users.noreply.github.com>
2023-10-20 11:35:24 -0400

    Update main.js

--- dsapractice/js/main.js
+++ dsapractice/js/main.js
@@ -1,4 +1,185 @@
 //DSA
+/*
+You are reading a Build Your Own Story book. It is like a normal book except that choices on some pages affect the story, sending you to one of two options for your next page.
+
+You start reading at page 1 and read forward one page at a time unless you reach a choice or an ending.
+
+The choices are provided in a list, sorted by the page containing the choice, and each choice has two options of pages to go to next. In this example, you are on page 3, where there is a choice. Option 1 goes to page 7 and Option 2 goes to page 8.
+
+choices1_1 = [[3, 7, 8], [9, 4, 2]] => [[current_page, option_1, option_2], ...]
+The ending pages are also given in a sorted list:
+
+endings1 = [6, 15, 21, 30]
+
+These choices are really stressing you out, so you decide that you'll either always pick the first option, or always pick the second option.
+
+Given a list of endings, a list of choices with their options, and the choice you will always take (Option 1 or Option 2), return the ending you will reach. If you get stuck in a loop, return -1.
+
+Example:
+stories(endings1, choices1_1, 1) (always Option 1)
+  Start: 1 -> 2 -> 3(choice) -> 7 -> 8 -> 9(choice) -> 4 -> 5 -> 6(end) => Return 6
+
+stories(endings1, choices1_1, 2) (always Option 2)
+  Start: 1 -> 2 -> 3(choice) -> 8 -> 9(choice) -> 2 -> 3(choice) -> 8 ... => Return -1
+
+Additional inputs:
+choices1_2 = [[3, 14, 2]]
+choices1_3 = [[5, 11, 28], [9, 19, 29], [14, 16, 20], [18, 7, 22], [25, 6, 30]]
+choices1_4 = [[2, 10, 15], [3, 4, 10], [4, 3, 15], [10, 3, 15]]
+
+endings2 = [11]
+choices2_1 = [[2, 3, 4], [5, 10, 2]]
+choices2_2 = []
+
+endings3 = [4, 11]
+choices3_1 = [[10, 6, 8]]
+
+Complexity Variable:
+n = number of pages
+(endings and choices are bound by the number of pages)
+
+All Test Cases :
+stories(endings1, choices1_1, 1) => 6
+stories(endings1, choices1_1, 2) => -1
+stories(endings1, choices1_2, 1) => 15
+stories(endings1, choices1_2, 2) => -1
+stories(endings1, choices1_3, 1) => 21
+stories(endings1, choices1_3, 2) => 30
+stories(endings1, choices1_4, 1) => -1
+stories(endings1, choices1_4, 2) => 15
+stories(endings2, choices2_1, 1) => 11
+stories(endings2, choices2_1, 2) => -1
+stories(endings2, choices2_2, 1) => 11
+stories(endings2, choices2_2, 2) => 11
+stories(endings3, choices3_1, 1) => 4
+stories(endings3, choices3_1, 2) => 4
+*/
+
+"use strict";
+
+
+const endings1 = [6, 15, 21, 30];
+const choices1_1 = [
+  [3, 7, 8],
+  [9, 4, 2]
+];
+
+const choices1_2 = [
+  [3, 14, 2]
+];
+const choices1_3 = [
+  [5, 11, 28],
+  [9, 19, 29],
+  [14, 16, 20],
+  [18, 7, 22],
+  [25, 6, 30]
+];
+const choices1_4 = [
+  [2, 10, 15],
+  [3, 4, 10],
+  [4, 3, 15],
+  [10, 3, 15]
+];
+
+const endings2 = [11];
+const choices2_1 = [
+  [2, 3, 4],
+  [5, 10, 2]
+];
+const choices2_2 = [];
+
+const endings3 = [4, 11];
+const choices3_1 = [
+  [10, 6, 8]
+];
+
+
+
+function stories(endings, choices, option) {
+  
+  
+  //create a variable representing max ending page
+  const maxPage = Math.max(...endings); //
+  
+  //list of past choices array
+  const pastChoices = [];
+  const choicePages = {};
+  
+  
+  for (let i = 0; i < choices.length; i++) {
+    choicePages[choices[i][0]] = choices[i][option];
+  }
+
+  for (let i = 1; i <= maxPage; i++ ) {
+    
+    //check if ending
+    if (endings.includes(i)) {
+      return i;
+    }
+    //check if in loop
+    
+    
+    if (choicePages[i]) {
+      
+      pastChoices.push(choicePages[i])
+      //check if in loop
+      if (pastChoices.includes(choicePages[i])) {
+        return -1;
+      }
+      
+      i = choicePages[i] // 7 
+
+    }
+    
+    
+  }
+  
+  
+  //cross check counter with list of pages that represent choice pages
+  
+  
+  //if a match, then go with the option given >> change counter to choice option
+  
+  //every time there is a match .. push that match into the past choices arr
+  
+  // if choice has been made more than once, you're in a loop
+  
+  //counter matches ending array >> ending reached
+  
+  
+  
+  
+  
+  return "done";
+}
+
+// console.log(storyTime(endings1, choices1_1, 1));
+
+
+
+// stories(endings1, choices1_1, 2) => -1
+console.log(stories(endings1, choices1_1, 2))
+
+
+// stories(endings1, choices1_2, 1) => 15
+// stories(endings1, choices1_2, 2) => -1
+// stories(endings1, choices1_3, 1) => 21
+// stories(endings1, choices1_3, 2) => 30
+// stories(endings1, choices1_4, 1) => -1
+// stories(endings1, choices1_4, 2) => 15
+// stories(endings2, choices2_1, 1) => 11
+// stories(endings2, choices2_1, 2) => -1
+// stories(endings2, choices2_2, 1) => 11
+// stories(endings2, choices2_2, 2) => 11
+// stories(endings3, choices3_1, 1) => 4
+// stories(endings3, choices3_1, 2) => 4
+
+
+// const endings1 = [6, 15, 21, 30];
+// const choices1_1 = [
+//   [3, 7, 8],
+//   [9, 4, 2]
+// ];
 //Flipping the Matrix
 
 function flippingMatrix(matrix) {
